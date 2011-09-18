15684d758ada9cfe8e2e36077e6e32d6e1f82648
blairkutz <blairkutz>
2011-09-18 05:32:58 +0000

    Going to sync local with GIT to see if everything will work being pulled from server.

--- src/user_installed/runGmailDelaySend.js
+++ src/user_installed/runGmailDelaySend.js
@@ -1,3 +1,5 @@
+var RUN_LOCAL_VERSION = true;
+
 var URLS = [];
 
 // TODO create one (compressed) script to download
@@ -13,7 +15,6 @@ var code_string = null;
 
 function getContext()
 {
- 
   if(!code_string)
     code_string = "";
   else
@@ -24,7 +25,7 @@ function getContext()
                     {
                       Logger.log("Getting URL:"+urlString);
                       var resp = UrlFetchApp.fetch(urlString);
-                      
+                      Logger.log("Response: "+resp.getResponseCode());
                       if(resp.getResponseCode() == 200)
                       {
                         Logger.log("Success with URL!");
@@ -39,6 +40,7 @@ function getContext()
 
   
   // ************* CODE START ****************//
+  Logger.log("Scripts to download: "+URLS);
   for(var i=0; i<URLS.length; i++)
   {
     var url = URLS[i];
@@ -69,37 +71,127 @@ function getContext()
   return code_string;
 }
 
-function runGmailDelaySend()
+function onEdit(event)
 {
-  eval(getContext());
-  main();
-}
-
-function onEdit()
-{
-  eval(getContext());
-  onEditContext();
+  Logger.log("Firing onEdit Trigger");
+  if(!RUN_LOCAL_VERSION)
+    eval(getContext());
+  onEditContext(event);
 }
 
 function onInstall()
 {
+  Logger.log("Firing onInstall Trigger");
   onOpen();
 }
 
 function onOpen()
 {
-  eval(getContext());
-  onOpenContext();
+  Logger.log("Firing onOpen Trigger");
+  createMenu();
 }
 
 function menuItemClear()
 {
-  eval(getContext());
+  if(!RUN_LOCAL_VERSION)
+    eval(getContext());
   clear();
 }
 
 function menuItemRestoreDefaults()
 {
-  eval(getContext());
+  if(!RUN_LOCAL_VERSION)
+    eval(getContext());
   restoreDefaults();
+  createNormalMenu();
+}
+
+function menuItemParseDate()
+{
+  var resp = Browser.inputBox("Enter date you would like to parse (eg. 'tomorrow' or 'Wednesday 10am')", Browser.Buttons.OK_CANCEL);
+  
+  //if(!RUN_LOCAL_VERSION)
+    eval(getContext());
+  
+  Logger.log("User Entered: "+resp);
+
+  var d = Date.parse(resp);
+  
+  Logger.log("Parsed value:"+d);
+  
+  if(d)
+    Browser.msgBox("Date string '"+resp+"' parsed successfully to date:\n\n"+new Date(d));
+  else
+    Browser.msgBox("Sorry, I couldn't understand '"+resp+"'");
+}
+
+function menuItemRunGmailDelaySendNow()
+{
+  if(!RUN_LOCAL_VERSION)
+    eval(getContext());
+  main();
+}
+
+function createMenu()
+{
+  if(firstTime())
+  {
+    //Browser.msgBox("Welcome to Gmail Delay Send! This spreadsheet contains your settings. Please see the '"+SCRIPT_NAME+"' menu for help");
+    //menuItemRestoreDefaults();
+    Browser.msgBox("Welcome to Gmail Delay Send. This spreadsheet will contain your setting once you install them through the "+SCRIPT_NAME+"->Install menu");
+    createFirstTimeMenu();
+  }
+  else
+  {
+    createNormalMenu();
+  }
+}
+
+function getSpreadsheet()
+{
+  return SpreadsheetApp.getActiveSpreadsheet();
+}
+
+function getSheet()
+{
+  return getSpreadsheet().getActiveSheet();
+}
+
+function createFirstTimeMenu()
+{
+  var ss = getSpreadsheet();
+  var menuEntries = [ {name: "Install", functionName: "menuItemRestoreDefaults"}];
+  ss.addMenu(SCRIPT_NAME, menuEntries);
+}
+
+function createNormalMenu()
+{
+  var ss = getSpreadsheet();
+  var menuEntries = [ {name: "Clear", functionName: "menuItemClear"},
+                      {name: "Run Now", functionName: "menuItemRunGmailDelaySendNow" },
+                      {name: "Test a Date", functionName: "menuItemParseDate" },
+                      {name: "Restore Defaults", functionName: "menuItemRestoreDefaults"},
+                      {name: "Help", functionName: "menuItemHelp"}];
+  ss.addMenu(SCRIPT_NAME, menuEntries);
+}
+
+function menuItemHelp()
+{
+  Browser.msgBox("Please visit the "+SCRIPT_NAME+" homepage at: http://code.google.com/p/gmail-delay-send for help");
+}
+
+// Hidden cell
+var HIDDEN_ROW = "20";
+var INSTALL_FLAG = "A"+HIDDEN_ROW;
+
+function firstTime()
+{
+  var toReturn = false;
+  var sheet = SpreadsheetApp.getActiveSheet();
+  
+  toReturn = !(sheet.getRange(INSTALL_FLAG).getValue().length > 0)
+
+  Logger.log("First time?:"+toReturn);
+  
+  return toReturn;
 }
