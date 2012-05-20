f2730fd4707b8bb7d3c11c3e4e5748baafd8167a
blairkutz <blairkutz@gmail.com>
2012-05-20 01:07:09 +0000

    7.6

--- src/downloaded/CustomDate.js
+++ src/downloaded/CustomDate.js
@@ -3,10 +3,20 @@ Date.today = function()
     if(this.relativeTo == null)
       return new Date().clearTime();
     else
-      return this.relativeTo.clearTime();
+      return this.relativeTo.clone().clearTime();
 }
 
 Date.setRelativeTo = function(d) 
 {
     this.relativeTo = d;
 }
+
+Date.prototype.setTimeToNow = function () 
+{
+    var n = Date.relativeTo || new Date();
+    this.setHours(n.getHours());
+    this.setMinutes(n.getMinutes());
+    this.setSeconds(n.getSeconds());
+    this.setMilliseconds(n.getMilliseconds());
+    return this;
+};
